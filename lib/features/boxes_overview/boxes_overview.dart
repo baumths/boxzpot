d0f1e2a560f2e3af67667b0343a05a15a36b098b
baumths <baumths@gmail.com>
2024-10-09 09:55:11 -0300

    add content to empty boxes overview screen

--- lib/features/boxes_overview/boxes_overview.dart
+++ lib/features/boxes_overview/boxes_overview.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../entities/box.dart';
 import '../../shared/box_title.dart';
 import '../../shared/boxes_store.dart';
 import '../box_details/box_details.dart';
@@ -11,28 +12,50 @@ class BoxesOverview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final store = context.watch<BoxesStore>();
     return Scaffold(
-      body: ListView(
-        children: [
-          for (final box in store.boxes)
-            ListTile(
-              title: BoxTitle(box: box),
-              trailing: const Icon(Icons.arrow_right_rounded),
-              onTap: () => Navigator.push<void>(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => BoxDetails(boxId: box.id),
-                ),
-              ),
-              onLongPress: () => BoxEditor.show(context, box: box),
-            ),
-        ],
-      ),
+      body: const BoxesList(),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () => BoxEditor.show(context),
       ),
+    );
+  }
+}
+
+class BoxesList extends StatelessWidget {
+  const BoxesList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final store = context.watch<BoxesStore>();
+
+    if (store.boxes.isEmpty) {
+      return Center(
+        child: Text(
+          '🗃️',
+          style: TextStyle(
+            fontSize: 128,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ),
+      );
+    }
+
+    return ListView(
+      children: [
+        for (final Box box in store.boxes)
+          ListTile(
+            title: BoxTitle(box: box),
+            trailing: const Icon(Icons.arrow_right_rounded),
+            onTap: () => Navigator.push<void>(
+              context,
+              MaterialPageRoute(
+                builder: (_) => BoxDetails(boxId: box.id),
+              ),
+            ),
+            onLongPress: () => BoxEditor.show(context, box: box),
+          ),
+      ],
     );
   }
 }
