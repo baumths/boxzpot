3e13fdcce73bc3d044f86a89be7f678d43a5360d
baumths <baumths@gmail.com>
2024-12-03 09:08:26 -0300

    improve empty boxes view

--- lib/features/boxes/boxes_overview.dart
+++ lib/features/boxes/boxes_overview.dart
@@ -30,14 +30,8 @@ class BoxesList extends StatelessWidget {
     final store = context.watch<BoxesStore>();
 
     if (store.boxes.isEmpty) {
-      return Center(
-        child: Text(
-          '🗃️',
-          style: TextStyle(
-            fontSize: 128,
-            color: Theme.of(context).colorScheme.primary,
-          ),
-        ),
+      return EmptyBoxesView(
+        onAddBoxPressed: () => BoxEditor.show(context),
       );
     }
 
@@ -56,6 +50,45 @@ class BoxesList extends StatelessWidget {
           ],
         );
       },
+    );
+  }
+}
+
+class EmptyBoxesView extends StatelessWidget {
+  const EmptyBoxesView({super.key, required this.onAddBoxPressed});
+
+  final VoidCallback onAddBoxPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '🗃️',
+              style: TextStyle(
+                fontSize: 128,
+                color: theme.colorScheme.primary,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              "Couldn't find any boxes around.\nLet's add a new one?",
+              textAlign: TextAlign.center,
+              style: theme.textTheme.titleLarge,
+            ),
+            const SizedBox(height: 16),
+            FilledButton(
+              onPressed: onAddBoxPressed,
+              child: const Text('Add Box'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
